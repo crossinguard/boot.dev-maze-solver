3b4deb3f8997042fbf886adc4e66a45c024090cf
crossinguard <w.brett.egbert@gmail.com>
2024-02-09 10:11:37 -0800

    maze solver base logic and functionality

--- cell.py
+++ cell.py
@@ -1,12 +1,17 @@
 from graphics import Line, Point
 
+bg_color = "#1d3557"
+grid_color = "#457b9d"
+correct_path_color = "#e63946"
+reset_path_color = "#6d0d15"
 
 class Cell:
-    def __init__(self, win):
+    def __init__(self, win=None):
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
+        self.visited = False
         self._x1 = None
         self._x2 = None
         self._y1 = None
@@ -23,15 +28,27 @@ class Cell:
         if self.has_left_wall:
             line = Line(Point(x1, y1), Point(x1, y2))
             self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line, bg_color)
         if self.has_top_wall:
             line = Line(Point(x1, y1), Point(x2, y1))
             self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line, bg_color)
         if self.has_right_wall:
             line = Line(Point(x2, y1), Point(x2, y2))
             self._win.draw_line(line)
+        else:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line, bg_color)
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line, bg_color)
 
     def draw_move(self, to_cell, undo=False):
         if self._win is None:
@@ -42,9 +59,9 @@ class Cell:
         to_x_mid = (to_cell._x1 + to_cell._x2) / 2
         to_y_mid = (to_cell._y1 + to_cell._y2) / 2
 
-        fill_color = "red"
+        fill_color = correct_path_color
         if undo:
-            fill_color = "gray"
+            fill_color = reset_path_color
 
         # moving left
         if self._x1 > to_cell._x1:
--- graphics.py
+++ graphics.py
@@ -1,11 +1,14 @@
 from tkinter import Tk, BOTH, Canvas
 
+bg_color = "#1d3557"
+grid_color = "#457b9d"
+
 class Window:
     def __init__(self, width, height):
         self.__root = Tk()
         self.__root.title("Maze Solver")
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
-        self.__canvas = Canvas(self.__root, bg="#171717", height=height, width=width)
+        self.__canvas = Canvas(self.__root, bg=bg_color, height=height, width=width)
         self.__canvas.pack(fill=BOTH, expand=1)
         self.__running = False
 
@@ -17,28 +20,32 @@ class Window:
         self.__running = True
         while self.__running:
             self.redraw()
+        print("window closed...")
 
-    def draw_line(self, line, fill_color = "#f2f2f2"):
+    def draw_line(self, line, fill_color=grid_color):
         line.draw(self.__canvas, fill_color)
-    
+
     def close(self):
         self.__running = False
+
 
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
-class Line:
-    def __init__(self, point1, point2):
-        self.point1 = point1
-        self.point2 = point2
 
-    def draw(self, canvas, fill_color = "#f2f2f2"):
+class Line:
+    def __init__(
+        self,
+        p1,
+        p2,
+    ):
+        self.p1 = p1
+        self.p2 = p2
+
+    def draw(self, canvas, fill_color=grid_color):
         canvas.create_line(
-            self.point1.x, self.point1.y,
-            self.point2.x, self.point2.y,
-            fill=fill_color,
-            width=2
+            self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
         )
         canvas.pack(fill=BOTH, expand=1)
